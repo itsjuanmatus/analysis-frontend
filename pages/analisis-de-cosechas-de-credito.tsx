f294f9149e5b48247699e9ed393faf71308a8763
itsjuanmatus <juanmatusg16@gmail.com>
2021-09-28 00:07:30 -0600

    sending json form

--- pages/analisis-de-cosechas-de-credito.tsx
+++ pages/analisis-de-cosechas-de-credito.tsx
@@ -2,8 +2,36 @@ import React, { useState, useEffect } from "react";
 import Sidebar from "../components/Layout/Sidebar";
 import UploadFiles from "../components/UploadFiles";
 import UploadDesembolsos from "../components/UploadDesembolsos";
+import storedProcedure from "./api/storedProcedure";
 
 export default function analisisDeCosechasDeCredito() {
+  const initialValues = {
+    fechaInicial: "2015-06-01",
+    fechaFinal: "2018-11-30",
+  };
+
+  const [value, setValue] = useState(initialValues);
+
+  const handleInputChange = (event: any) => {
+    const { name, value } = event.target;
+    setValue({ ...value, [name]: value });
+  };
+
+  const saveValue = () => {
+    var data = {
+      fechaInicial: value.fechaInicial,
+      fechaFinal: value.fechaFinal,
+    };
+
+    storedProcedure.create(data)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
   return (
     <div className="flex min-h-screen m-auto w-full">
       {<Sidebar />}
@@ -28,8 +56,42 @@ export default function analisisDeCosechasDeCredito() {
             </h3>
             <p className="mb-4">Columnas requeridas en esta tabla: id, ...</p>
             <UploadDesembolsos />
+
+            <div className="submit-form">
+              <div>
+                <div className="form-group">
+                  <label htmlFor="fechaInicial">Fecha Inicial</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="fechaInicial"
+                    required
+                    value={value.fechaInicial}
+                    onChange={handleInputChange}
+                    name="fechaInicial"
+                  />
+                </div>{" "}
+                <div className="form-group">
+                  <label htmlFor="fechaFinal">Fecha Final</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="fechaFinal"
+                    required
+                    value={value.fechaFinal}
+                    onChange={handleInputChange}
+                    name="fechaFinal"
+                  />
+                </div>
+                <button onClick={saveValue} className="btn btn-success">
+                  Submit
+                </button>
+              </div>
+            </div>
           </div>
         </div>
+
+        <div></div>
       </main>
     </div>
   );
