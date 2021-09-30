309a752accf02f45dd594c26406c4a7541817ba3
itsjuanmatus <juanmatusg16@gmail.com>
2021-09-29 20:38:35 -0600

    added docker

---
+++ pages/riesgos_crediticios/scoring_de_originacion_crediticia.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Sidebar from "../../components/Layout/Sidebar";
+
+export default function scoring_de_originacion_crediticia() {
+  return (
+    <div className="flex min-h-screen m-auto w-full">
+      {<Sidebar />}
+      <main className="m-10 w-full">
+        <div className="flex flex-col justify-center  p-10 border  w-full">
+          <h2 className="font-bold text-2xl mb-10">
+            Scoring de originación crediticia
+          </h2>
+        </div>
+      </main>
+    </div>
+  );
+}
