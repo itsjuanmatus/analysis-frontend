050da849006af221458720979737b817fbb74f98
itsjuanmatus <juanmatusg16@gmail.com>
2021-10-05 21:04:32 -0600

    added floating to tabla scoring

--- pages/riesgos_crediticios/scoring_de_originacion_crediticia/tabla_scoring.tsx
+++ pages/riesgos_crediticios/scoring_de_originacion_crediticia/tabla_scoring.tsx
@@ -8,6 +8,7 @@ import Dropdown from '../../../components/UI/Dropdown'
 import SacCalculoScore from '../../api/riesgos_crediticios/scoring_de_originacion_crediticia/stored_procedures/sac_calculo_score'
 import Table, { StatusPill } from '../../../components/UI/Table'
 import dropdownData from '../../../components/data/dropdowns/riesgos_crediticios/scoring'
+import FloatingBackground from '../../../components/UI/FloatingBackground'
 
 function TablaScoring ({ tableData }: any) {
   const router = useRouter()
@@ -20,8 +21,6 @@ function TablaScoring ({ tableData }: any) {
   const refreshData = () => {
     router.replace(router.asPath)
   }
-
-  
 
   const [message, setMessage] = React.useState('')
 
@@ -84,39 +83,31 @@ function TablaScoring ({ tableData }: any) {
   )
 
   return (
-    <div className='flex min-h-screen m-auto w-full'>
-      {<Sidebar />}
-      <main className='m-10 w-full flex justify-center '>
-        <div className='flex-col justify-start p-10 border border-t border-gray-200 rounded-md w-full max-w-5xl'>
-          <h2 className='font-bold text-2xl mb-5'>
-            Scoring de originación crediticia
-          </h2>
-          <div className='max-w-sm mb-10'>
-            <Dropdown dropdownData={dropdownData} />
-          </div>
-          <div className='flex space-x-10'>
-            <div className='max-w-xs'>
-              <h3 className='font-semibold text-xl'>
-                Generar datos de Analisis
-              </h3>
-              <p className='text-gray-500'>
-                Asegurate de haber subido los catalogos correctamente
-              </p>
-              <button
-                onClick={saveValue}
-                className='p-2 bg-indigo-600 rounded text-white px-4 max-w-max mt-4 inline-flex items-center'
-              >
-                <LoadingIndicator />
-                Enviar
-              </button>
-            </div>
-          </div>
-          <div className='w-full mt-10 mb-20'>
-            <Table columns={columns} data={fields} />
-          </div>
+    <FloatingBackground
+      sidebar={<Sidebar />}
+      dropdownData={dropdownData}
+      title='Scoring de originación crediticia'
+      subtitle='Generar analisis'
+    >
+      <div className='flex space-x-10'>
+        <div className='max-w-xs'>
+          <h3 className='font-semibold text-xl'>Generar datos de Analisis</h3>
+          <p className='text-gray-500'>
+            Asegurate de haber subido los catalogos correctamente
+          </p>
+          <button
+            onClick={saveValue}
+            className='p-2 bg-indigo-600 rounded text-white px-4 max-w-max mt-4 inline-flex items-center'
+          >
+            <LoadingIndicator />
+            Enviar
+          </button>
         </div>
-      </main>
-    </div>
+      </div>
+      <div className='w-full mt-10 mb-20'>
+        <Table columns={columns} data={fields} />
+      </div>
+    </FloatingBackground>
   )
 }
 
